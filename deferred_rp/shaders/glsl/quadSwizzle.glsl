e44640a3449d10632b55b06852e8c5fe21418a7a
Veka0 <26331365+Veka0@users.noreply.github.com>
2023-03-26 10:44:37 +0200

    even more fixes

--- deferred_rp/shaders/glsl/quadSwizzle.glsl
+++ deferred_rp/shaders/glsl/quadSwizzle.glsl
@@ -13,11 +13,6 @@
 // functions, and it can be pre-computed and passed into
 // functions for better performance.
 
-// How to compute swizzle quad position:
-// x = mod(gl_FragCoord.x - 0.5, 2.0)
-// y = mod(gl_FragCoord.y - 0.5, 2.0)
-// z = dFfx(x*y)
-
 
 // Quad swizzling index
 // Swizzling index is an integer value between 0 and 3 that
@@ -39,29 +34,21 @@
 vec3 quadSwizzlePos() {
 	vec3 p;
 	p.xy = mod(gl_FragCoord.xy-0.5, 2.0);
+	p.x = dFdx(p.x) == -1.0 ? 1.0-p.x : p.x;
+	p.y = dFdy(p.y) == -1.0 ? 1.0-p.y : p.y;
 	p.z = dFdx(p.x*p.y);
 	return p;
-}
-
-// Returns swizzling index - 0 to 3 (swizzling of 3 is unsupported).
-int quadSwizzleIndex(vec3 pos) {
-	int index = 3;
-	float val = dot(pos.xy, vec2(1, 2));
-	index = pos.x == 1.0 && pos.y == pos.z ? 0 : index;
-	index = pos.x == 0.0 && pos.y == pos.z ? 1 : index;
-	index = pos.z != pos.y && dFdy(pos.x*pos.y) == pos.x ? 2 : index;
-	return index;
 }
 
 
 // Swizzling functions
 
 // float
-float quadSwizzle0(float value) { return dFdx(mod(gl_FragCoord.x - 0.5, 2.0)*value); }
+float quadSwizzle0(float value) { float p = mod(gl_FragCoord.x-0.5, 2.0); return dFdx((dFdx(p) == -1.0 ? 1.0-p : p)*value); }
 float quadSwizzle0(float value, vec3 pos) { return dFdx(pos.x*value); }
 float quadSwizzle0(float value, float pos_x) { return dFdx(pos_x*value); }
 
-float quadSwizzle1(float value) { return -dFdx((mod(gl_FragCoord.x + 0.5, 2.0))*value); }
+float quadSwizzle1(float value) { float p = mod(gl_FragCoord.x-0.5, 2.0); return -dFdx((dFdx(p) == 1.0 ? 1.0-p : p)*value); }
 float quadSwizzle1(float value, vec3 pos) { return -dFdx((1.0-pos.x)*value); }
 float quadSwizzle1(float value, float pos_x) { return -dFdx((1.0-pos_x)*value); }
 
@@ -70,11 +57,11 @@ float quadSwizzle2(float value, vec3 pos) { return mix(dFdy(pos.y*value), -dFdy(
 float quadSwizzle2(float value, vec2 pos_yz) { return mix(dFdy(pos_yz.x*value), -dFdy((1.0-pos_yz.x)*value), pos_yz.y); }
 
 // vec2
-vec2 quadSwizzle0(vec2 value) { return dFdx(mod(gl_FragCoord.x - 0.5, 2.0)*value); }
+vec2 quadSwizzle0(vec2 value) { float p = mod(gl_FragCoord.x-0.5, 2.0); return dFdx((dFdx(p) == -1.0 ? 1.0-p : p)*value); }
 vec2 quadSwizzle0(vec2 value, vec3 pos) { return dFdx(pos.x*value); }
 vec2 quadSwizzle0(vec2 value, float pos_x) { return dFdx(pos_x*value); }
 
-vec2 quadSwizzle1(vec2 value) { return -dFdx((mod(gl_FragCoord.x + 0.5, 2.0))*value); }
+vec2 quadSwizzle1(vec2 value) { float p = mod(gl_FragCoord.x-0.5, 2.0); return -dFdx((dFdx(p) == 1.0 ? 1.0-p : p)*value); }
 vec2 quadSwizzle1(vec2 value, vec3 pos) { return -dFdx((1.0-pos.x)*value); }
 vec2 quadSwizzle1(vec2 value, float pos_x) { return -dFdx((1.0-pos_x)*value); }
 
@@ -83,11 +70,11 @@ vec2 quadSwizzle2(vec2 value, vec3 pos) { return mix(dFdy(pos.y*value), -dFdy((1
 vec2 quadSwizzle2(vec2 value, vec2 pos_yz) { return mix(dFdy(pos_yz.x*value), -dFdy((1.0-pos_yz.x)*value), pos_yz.y); }
 
 // vec3
-vec3 quadSwizzle0(vec3 value) { return dFdx(mod(gl_FragCoord.x - 0.5, 2.0)*value); }
+vec3 quadSwizzle0(vec3 value) { float p = mod(gl_FragCoord.x-0.5, 2.0); return dFdx((dFdx(p) == -1.0 ? 1.0-p : p)*value); }
 vec3 quadSwizzle0(vec3 value, vec3 pos) { return dFdx(pos.x*value); }
 vec3 quadSwizzle0(vec3 value, float pos_x) { return dFdx(pos_x*value); }
 
-vec3 quadSwizzle1(vec3 value) { return -dFdx((mod(gl_FragCoord.x + 0.5, 2.0))*value); }
+vec3 quadSwizzle1(vec3 value) { float p = mod(gl_FragCoord.x-0.5, 2.0); return -dFdx((dFdx(p) == 1.0 ? 1.0-p : p)*value); }
 vec3 quadSwizzle1(vec3 value, vec3 pos) { return -dFdx((1.0-pos.x)*value); }
 vec3 quadSwizzle1(vec3 value, float pos_x) { return -dFdx((1.0-pos_x)*value); }
 
@@ -96,11 +83,11 @@ vec3 quadSwizzle2(vec3 value, vec3 pos) { return mix(dFdy(pos.y*value), -dFdy((1
 vec3 quadSwizzle2(vec3 value, vec2 pos_yz) { return mix(dFdy(pos_yz.x*value), -dFdy((1.0-pos_yz.x)*value), pos_yz.y); }
 
 // vec4
-vec4 quadSwizzle0(vec4 value) { return dFdx(mod(gl_FragCoord.x - 0.5, 2.0)*value); }
+vec4 quadSwizzle0(vec4 value) { float p = mod(gl_FragCoord.x-0.5, 2.0); return dFdx((dFdx(p) == -1.0 ? 1.0-p : p)*value); }
 vec4 quadSwizzle0(vec4 value, vec3 pos) { return dFdx(pos.x*value); }
 vec4 quadSwizzle0(vec4 value, float pos_x) { return dFdx(pos_x*value); }
 
-vec4 quadSwizzle1(vec4 value) { return -dFdx((mod(gl_FragCoord.x + 0.5, 2.0))*value); }
+vec4 quadSwizzle1(vec4 value) { float p = mod(gl_FragCoord.x-0.5, 2.0); return -dFdx((dFdx(p) == 1.0 ? 1.0-p : p)*value); }
 vec4 quadSwizzle1(vec4 value, vec3 pos) { return -dFdx((1.0-pos.x)*value); }
 vec4 quadSwizzle1(vec4 value, float pos_x) { return -dFdx((1.0-pos_x)*value); }
 
@@ -108,3 +95,10 @@ vec4 quadSwizzle2(vec4 value) { vec3 p = quadSwizzlePos(); return mix(dFdy(p.y*v
 vec4 quadSwizzle2(vec4 value, vec3 pos) { return mix(dFdy(pos.y*value), -dFdy((1.0-pos.y)*value), pos.z); }
 vec4 quadSwizzle2(vec4 value, vec2 pos_yz) { return mix(dFdy(pos_yz.x*value), -dFdy((1.0-pos_yz.x)*value), pos_yz.y); }
 
+// Returns swizzling index - 0 to 3 (swizzling of 3 is unsupported).
+int quadSwizzleIndex(vec3 pos) {
+	int index = pos == quadSwizzle0(pos, pos) ? 0 : 3;
+	index = pos == quadSwizzle1(pos, pos) ? 1 : index;
+	index = pos == quadSwizzle2(pos, pos) ? 2 : index;
+	return index;
+}
